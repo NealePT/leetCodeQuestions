0870f2439c5925f2966fbc8585009d013d843a64
Neale Taylor <nealeptaylor@gmail.com>
2022-04-20 15:13:49 -0400

    add solution to ransom note

--- 383-ransom-note.js
+++ 383-ransom-note.js
@@ -1,3 +1,19 @@
-const canConstruct = function(ransomNote) {
-  
+const canConstruct = function(ransomNote, magazine) {
+  if (ransomNote.length > magazine.length) {
+    return false;
+  }
+  let ransomNoteArr = ransomNote.split('');
+  let oldMagazineLength = magazine.length;
+  ransomNoteArr.forEach(function(item, index) {
+    magazine = magazine.replace(item,'');
+  });
+  if (oldMagazineLength == magazine.length + ransomNoteArr.length) {
+    return true;
+  } else {
+    return false;
+  }
 };
+
+console.log(canConstruct('a', 'b'));
+console.log(canConstruct('aa', 'ab'));
+console.log(canConstruct('aa', 'aab'));
